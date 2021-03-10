24260cdcab5dfc28618d5c14802c1f762fdd07ef
yn-s19 <yn-s19@DESKTOP-FA6CS7S>
2021-03-10 10:37:16 +0900

    step1

---
+++ reserve/src/controller/reservFrontController.java
@@ -0,0 +1,89 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action;
+import vo.ActionForward;
+
+/**
+ * Servlet implementation class reservFrontController
+ */
+@WebServlet(urlPatterns = {"*.mem","own","log"})
+public class reservFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public reservFrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	
+		request.setCharacterEncoding("utf-8");
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		ActionForward forward = null;
+		Action action = null;
+		System.out.println(command);
+		
+		if(command.equals("/loginForm.log")) {
+			forward = new ActionForward();
+			forward.setPath("/member/loginForm.jsp");
+		}
+		else if(command.equals("/loginProcess.log")) {
+			action = new LoginAction();
+			try {
+				forward = action.execute(request, response);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/joinForm.mem")) {
+			forward = new ActionForward();
+			forward.setPath("/member/joinForm.jsp");
+		} 
+		else if(command.equals("/joinProcess.mem")) {
+			action = new JoinProAction();
+			try {
+				forward = action.execute(request, response);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(forward != null) {
+			if(forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			}else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+
+}
