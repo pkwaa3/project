e9772929a0e3a5949833d2e9dce1189bf4012d02
yn-s20 <yn-s20@DESKTOP-DNF21AL>
2021-03-10 10:18:50 +0900

    vo

--- reserve/src/vo/Owner.java
+++ reserve/src/vo/Owner.java
@@ -1,5 +1,63 @@
 package vo;
 
 public class Owner {
-
+	private String owner_id;
+	private String owner_pw;
+	private String owner_name;
+	private int owner_age;
+	private String owner_gender;
+	private int owner_number;
+	private String owner_eamil;
+	private int owner_no;
+	
+	public String getOwner_id() {
+		return owner_id;
+	}
+	public void setOwner_id(String owner_id) {
+		this.owner_id = owner_id;
+	}
+	public String getOwner_pw() {
+		return owner_pw;
+	}
+	public void setOwner_pw(String owner_pw) {
+		this.owner_pw = owner_pw;
+	}
+	public String getOwner_name() {
+		return owner_name;
+	}
+	public void setOwner_name(String owner_name) {
+		this.owner_name = owner_name;
+	}
+	public int getOwner_age() {
+		return owner_age;
+	}
+	public void setOwner_age(int owner_age) {
+		this.owner_age = owner_age;
+	}
+	public String getOwner_gender() {
+		return owner_gender;
+	}
+	public void setOwner_gender(String owner_gender) {
+		this.owner_gender = owner_gender;
+	}
+	public int getOwner_number() {
+		return owner_number;
+	}
+	public void setOwner_number(int owner_number) {
+		this.owner_number = owner_number;
+	}
+	public String getOwner_eamil() {
+		return owner_eamil;
+	}
+	public void setOwner_eamil(String owner_eamil) {
+		this.owner_eamil = owner_eamil;
+	}
+	public int getOwner_no() {
+		return owner_no;
+	}
+	public void setOwner_no(int owner_no) {
+		this.owner_no = owner_no;
+	}
+	
+	
 }
--- reserve/src/vo/Reservation.java
+++ reserve/src/vo/Reservation.java
@@ -1,5 +1,51 @@
 package vo;
 
-public class Reservation {
+import java.sql.Date;
 
+public class Reservation {
+	private int reserv_no;
+	private int member_no;
+	private int rest_no;
+	private Date date;
+	private int time;
+	private int head;
+	
+	public int getReserv_no() {
+		return reserv_no;
+	}
+	public void setReserv_no(int reserv_no) {
+		this.reserv_no = reserv_no;
+	}
+	public int getMember_no() {
+		return member_no;
+	}
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+	public int getRest_no() {
+		return rest_no;
+	}
+	public void setRest_no(int rest_no) {
+		this.rest_no = rest_no;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public int getHead() {
+		return head;
+	}
+	public void setHead(int head) {
+		this.head = head;
+	}
+	
+	
 }
