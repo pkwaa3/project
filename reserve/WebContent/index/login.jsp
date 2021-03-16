5caf74e04d29734e90eaab347e470dea9a68340f
yn-s19 <yn-s19@DESKTOP-FA6CS7S>
2021-03-16 16:27:50 +0900

    xxx

--- reserve/WebContent/index/login.jsp
+++ reserve/WebContent/index/login.jsp
@@ -8,12 +8,8 @@
 </head>
 <body>
 <a href="../company/logincom.jsp">관리자</a>
-<a href="../owner/loginow.jsp">점장</a>
-<<<<<<< HEAD
+<a href="../ownerLoginForm.own">점장</a>
 <a href="../memberLoginForm.mem">일반</a>
-=======
-<a href="../memberLoginform.mem">일반</a>
 
->>>>>>> branch 'master' of https://github.com/pkwaa3/project.git
 </body>
 </html>
--- reserve/src/owner/action/ownerJoinAction.java
+++ reserve/src/owner/action/ownerJoinAction.java
@@ -38,14 +38,14 @@ public class ownerJoinAction implements Action {
    			out.println("</script>");
 	   	}
    		else{
-   	    forward = new ActionForward();
+   	    
    		response.setContentType("text/html;charset=utf-8");
 		
    		PrintWriter out = response.getWriter();
 		
 		out.println("<script>");
 		out.println("alert('회원가입하였습니다.');");
-		out.println("location.herf='ownerLoginForm.own'");
+		out.println("location.href='ownerLoginForm.own'");
 		out.println("</script>");
    		}
    		return forward;
